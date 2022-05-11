8c65211aaffb538ebcf2643f95f8fb5b354ec318
JohnnyShihHg <johnny.shih1997@gmail.com>
2022-05-11 15:23:44 +0800

    original http test url

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 // console.log('Hello Node.js look here!');
-const hello=require('./hello')
+// const hello=require('./hello')
+// hello.helloapp()
+const http = require('http');
 
-hello.helloapp()
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        return res.end('This is home page');
+    } 
+		if (req.url === '/login') {
+        return res.end('This is login page'); //當url是/login 回傳到頁面上
+    } 
+    res.end('page not found :(');
+});
+
+server.listen(3000, () => {
+	console.log('running server on port 3000');
+});
